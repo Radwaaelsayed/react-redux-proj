d4c893f499dd0e2752577573f1fd25818ec79b57
Radwa Elsayed <69940198+Radwaaelsayed@users.noreply.github.com>
2020-12-06 04:19:40 -0800

    Update notfoundpage.js

--- src/Components/notfoundpage.js
+++ src/Components/notfoundpage.js
@@ -2,7 +2,7 @@ import React from 'react';
 const NotFoundPage = () =>{
     return(
         <div>
-            <img src=".././images/notfound.png" className="w-50" style={{margin:"auto"}} />
+            <img src=".././images/notfound.png" className="w-50" style={{margin:"auto"}}  alt=""/>
         </div>
     )
 }
